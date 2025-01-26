8a3a78c58a05177e4ecd0e374baba1292a26af99
Sowilo-Bear <echowellq@gmail.com>
2025-01-26 01:54:18 +0000

    TTS

---
+++ Assets/Sowilo/Script/VoiceManager.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+using UnityEngine.UI;
+using Oculus.Voice;
+using System.Reflection;
+using Meta.WitAi.CallbackHandlers;
+
+public class VoiceManager : MonoBehaviour
+{
+    [Header("Wit Configuration")]
+    [SerializeField] private AppVoiceExperience appVoiceExperience;
+    [SerializeField] private WitResponseMatcher responseMatcher;
+    //[SerializeField] private TextMeshProUGUI transcriptionText;
+    [SerializeField] private InputField transcriptionText;
+
+    [Header("Voice Events")]
+    //[SerializeField] private AmazonBedrockConnection amazonBedrockConnection;
+    [SerializeField] private UnityEvent wakeWordDetected;
+    [SerializeField] private UnityEvent<string> completeTranscription;
+
+    private bool _voiceCommandReady;
+
+    private void Awake()
+    {
+        appVoiceExperience.VoiceEvents.OnRequestCompleted.AddListener(ReactivateVoice);
+        appVoiceExperience.VoiceEvents.OnPartialTranscription.AddListener(OnPartialTranscription);
+        appVoiceExperience.VoiceEvents.OnFullTranscription.AddListener(OnFullTranscription);
+
+        var eventField = typeof(WitResponseMatcher).GetField("onMultiValueEvent", BindingFlags.NonPublic | BindingFlags.Instance);
+        if (eventField != null && eventField.GetValue(responseMatcher) is MultiValueEvent onMultiValueEvent)
+        {
+            onMultiValueEvent.AddListener(WakeWordDetected);
+        }
+
+        appVoiceExperience.Activate();
+    }
+
+    private void OnDestroy()
+    {
+        appVoiceExperience.VoiceEvents.OnRequestCompleted.RemoveListener(ReactivateVoice);
+        appVoiceExperience.VoiceEvents.OnPartialTranscription.RemoveListener(OnPartialTranscription);
+        appVoiceExperience.VoiceEvents.OnFullTranscription.RemoveListener(OnFullTranscription);
+
+
+        var eventField = typeof(WitResponseMatcher).GetField("onMultiValueEvent", BindingFlags.NonPublic | BindingFlags.Instance);
+        if (eventField != null && eventField.GetValue(responseMatcher) is MultiValueEvent onMultiValueEvent)
+        {
+            onMultiValueEvent.RemoveListener(WakeWordDetected);
+        }
+    }
+
+    private void ReactivateVoice() => appVoiceExperience.Activate();
+
+    private void WakeWordDetected(string[] arg0)
+    {
+        _voiceCommandReady = true;
+        wakeWordDetected?.Invoke();
+    }
+
+    private void OnPartialTranscription(string transcription)
+    {
+        if (!_voiceCommandReady) return;
+        transcriptionText.text = transcription;
+    }
+
+    private void OnFullTranscription(string transcription)
+    {
+        if (!_voiceCommandReady) return;
+        _voiceCommandReady = false;
+        //amazonBedrockConnection.SendPrompt(transcription);
+        completeTranscription?.Invoke(transcription);
+    }
+}
